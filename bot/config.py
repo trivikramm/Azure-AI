3f3b6f71b46fb17e1ed7e2e0a220007d0b511445
trivikramm <76730493+trivikramm@users.noreply.github.com>
2024-12-23 21:04:42 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 class DefaultConfig:
     PORT = 3978
-    APP_ID = os.getenv("MICROSOFT_APP_ID", "5d11f002-b277-4231-a5df-61516f3a010a")
-    APP_PASSWORD = os.getenv("MICROSOFT_APP_PASSWORD", "52b6f7ce-6aee-43ca-b583-a211c2da8c4d")
-    LUIS_APP_ID = os.getenv("LUIS_APP_ID", "8bd16ad0-b10c-41bf-b8a0-5418b7ce2090")
-    LUIS_API_KEY = os.getenv("LUIS_API_KEY", "9b333b87e2f24b57aec64c4ec78c1441")
+    APP_ID = os.getenv("MICROSOFT_APP_ID", "")
+    APP_PASSWORD = os.getenv("MICROSOFT_APP_PASSWORD", "")
+    LUIS_APP_ID = os.getenv("LUIS_APP_ID", "")
+    LUIS_API_KEY = os.getenv("LUIS_API_KEY", "")
     LUIS_API_HOST_NAME = os.getenv("LUIS_API_HOST_NAME", "luis-ai.cognitiveservices.azure.com/")
